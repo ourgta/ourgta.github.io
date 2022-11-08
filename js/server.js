5948a95023d827ea4f8f2edcc99693ae6ac5b519
ourgta <117709383+ourgta@users.noreply.github.com>
2022-11-08 22:58:59 +0000

    Add files via upload

---
+++ js/server.js
@@ -0,0 +1,30 @@
+(async () => {
+  const hello = "Hello world!";
+  console.log(hello);
+
+  const params = new URLSearchParams(document.location.search);
+  const sid = params.get("sid");
+
+  if (!sid) {
+    return;
+  }
+
+  const response = await fetch(
+    `https://api.battlemetrics.com/servers/${sid}?include=player`
+  );
+  const data = await response.json();
+
+  const list = document.getElementById("players");
+
+  for (const player of data.included.sort((a, b) =>
+    a.attributes.name > b.attributes.name ? 1 : -1
+  )) {
+    const anchor = document.createElement("a");
+    anchor.href = `/player.html?pid=${player.attributes.id}`;
+    anchor.appendChild(document.createTextNode(player.attributes.name));
+
+    const listItem = document.createElement("li");
+    listItem.appendChild(anchor);
+    list.appendChild(listItem);
+  }
+})();
