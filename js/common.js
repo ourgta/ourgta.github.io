b92b109681d33a3e92e2d4c418eece667374ae97
ourgta <117709383+ourgta@users.noreply.github.com>
2023-06-16 12:14:46 +0100

    Initial commit

---
+++ js/common.js
@@ -0,0 +1,30 @@
+function parseDate(timestamp) {
+  if (!timestamp) return "";
+
+  const date = new Date(timestamp);
+  return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+}
+
+function timeBetween(start, end) {
+  const milliseconds = end - start;
+  const seconds = Math.floor((milliseconds / 1000) % 60);
+  const minutes = Math.floor((milliseconds / 1000 / 60) % 60);
+  const hours = Math.floor((milliseconds / 1000 / 60 / 60) % 24);
+
+  return [
+    hours.toString().padStart(2, "0"),
+    minutes.toString().padStart(2, "0"),
+    seconds.toString().padStart(2, "0"),
+  ].join(":");
+}
+
+function rateLimit(headers) {
+  const url = new URL("rate-limit.html", window.location.href);
+  url.searchParams.set("url", window.location.href);
+  url.searchParams.set(
+    "date",
+    new Date(new Date().getTime() + headers.get("ratelimit-reset") * 1000)
+  );
+  window.location.href = url;
+  return;
+}
